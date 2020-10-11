93901d4d2effe5d575c7f31a0b145c8a75efb77c
Tyler McClanahan <tmak1787@gmail.com>
2020-10-11 11:18:36 -0600

    not sure what all the changes are; hospital sync

--- scripts/bitwarden.sh
+++ scripts/bitwarden.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #flatpak run com.bitwarden.desktop &
-/home/tmak/builds/Bitwarden-1.22.1-x86_64.AppImage &> /dev/null &
+/home/tmak/builds/Bitwarden-1.22.2-x86_64.AppImage &> /dev/null &
---
+++ scripts/single.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --off --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
