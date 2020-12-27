abe08f3c213271538a893277f3412169b1c47bb9
Tyler McClanahan <tmak1787@gmail.com>
2020-12-27 13:50:40 -0700

    update vimrc for linebreak and textwidth values; add singleleft and right screen scripts

--- scripts/single.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-I-1 --off --output DP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
---
+++ scripts/singleleft.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
