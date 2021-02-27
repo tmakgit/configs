e0270f795516c5c4e2f094bad2d4473a72dd8f97
Tyler McClanahan <tmak1787@gmail.com>
2021-02-27 07:08:25 -0700

    remove galileo scripts

--- scripts/bitwarden.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-snap run bitwarden &> /dev/null &
--- scripts/mattermost.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-snap run mattermost-desktop &> /dev/null &
-#/home/tmak/builds/mattermost-desktop-4.6.1-linux-x64/mattermost-desktop &> /dev/null &
